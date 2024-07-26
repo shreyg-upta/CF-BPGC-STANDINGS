8760ba2c9a00dc6e84fcba3aa91e3232aeb56d8d
Prakhar Bhandari <prakharb2k6@gmail.com>
2024-07-26 21:08:52 +0530

    made it even more simpler....just a list of dicts with all the data....and u can just add more stuff in it, to get the work done

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+data = [
+  {
+    name: "BPGC23",
+    id: "2acacdb25f059dc84d6d7f833a0c8abb",
+  },
+  {
+    name: "SPOJmentees",
+    id: "8122dc0cffa3c2693f67c5e0ccc912b7",
+  },
+  {
+    name: "QSTP24",
+    id: "9c014df584a90ac222d19d8c404ddeef",
+  },
+];
+
 document.addEventListener("DOMContentLoaded", function () {
-  const bpgc23Button = document.getElementById("bpgc23Button");
-  const spojMBButton = document.getElementById("spojMButton");
-  const qstpButton = document.getElementById("QSTP24Button");
+  const options = document.getElementById("options");
 
   function handleButtonClick(button) {
     button.addEventListener("click", function () {
@@ -25,21 +38,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  if (bpgc23Button) {
-    handleButtonClick(bpgc23Button);
-  } else {
-    console.error("bpgc23Button with ID 'bpgc23Button' not found.");
-  }
-
-  if (spojMBButton) {
-    handleButtonClick(spojMBButton);
-  } else {
-    console.error("spojMBButton with ID 'spojMButton' not found.");
-  }
-
-  if (qstpButton) {
-    handleButtonClick(qstpButton);
-  } else {
-    console.error("qstpButton with ID 'QSTP24Button' not found.");
+  for (let i = 0; i < data.length; i++) {
+    const button = document.createElement("button");
+    button.textContent = data[i].name;
+    button.value = data[i].id;
+    button.id = data[i].name + "Button";
+    button.classList.add("btn");
+    button.classList.add("btn-dark");
+    options.appendChild(button);
+    if (button) {
+      handleButtonClick(button);
+    } else {
+      console.error(button.name + "not found.");
+    }
   }
 });
